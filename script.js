bfde738341c445d2ec2ca1b290f6480e78f0b42d
MadamButerbrod <abso.mail@gmail.com>
2025-03-24 01:09:54 +0200

    update inventory

--- script.js
+++ script.js
@@ -1,42 +1,56 @@
-/**
- * @param {object} jq - JQery object of an item, default is all elements with class .item
- */
-class item {
-    constructor (jq = $('.item')) {
-        this.jq = jq;
-        this.weight = this.jq.data('weight');
-    }
-}
 class inventory {
     constructor () {
         this.inventory = [];
     }
     /**
+     * @param {JQuery} i
+     */
+    set inTrans (i) {
+        if (!(i.data('id'))) throw 'this item has no data-id attribute';
+        this._inTransition = i;
+        this._indexInInventory = this.inventory.findIndex(cat => this._inTransition.data('id') === cat.item.data('id'));
+    }
+    get inTrans () {
+        return this._inTransition
+    }
+    /**
      * add new item to inventory
-     * @param {item} newItem 
+     * @param {JQuery} newItem 
      * @param {number} newQuantity 
      */
     addItem (newItem, newQuantity) {
-        if (this.inventory.length != 0) {
-            let i = this.inventory.findIndex(cat => newItem.jq.attr('id') === cat.item.jq.attr('id'));
+            if (!(newItem.data('id'))) throw 'newItem has no data-id attribute';
+            let i = this.inventory.findIndex(cat => newItem.data('id') === cat.item.data('id'));
             if (i != -1) {
                 this.inventory[i].quantity += newQuantity;
+                let upitem = $(`#inventory>[data-id='${newItem.data('id')}']`);
+                let upquant = upitem.next();
                 if (this.inventory[i].quantity <= 0) {
-                    delete this.inventory[i]
-                } return
+                    this.inventory.splice(i, 1);
+                    upitem.remove();
+                    upquant.remove();
+                }
+                else {
+                    upquant.text(`x${this.inventory[i].quantity}`);
+                }
             } 
-        }
-        let newCategory = {
-                item: newItem,
-                quantity: newQuantity,
-        };
-        this.inventory.push(newCategory);
+            else {
+                let newCategory = {
+                        item: newItem,
+                        quantity: newQuantity,
+                };
+                this.inventory.push(newCategory);
+                $('#inventory').append(newItem);
+                $('#inventory').append(`<p class='count'>x${newQuantity}</p>`);
+                return newItem
+            }
+
     }
-    get jq () {
-        return $('#inventory')
+    getCurrQuantity () {
+        if(this._indexInInventory != -1) {return this.inventory[this._indexInInventory].quantity}
+        else {return 0}
     }
 }
-
 function show (block) {
     if (!(block.hasClass('show'))) {
         block.addClass('show');
@@ -48,19 +62,33 @@ function hide (block) {
     }
 }
 
-
+let inv = new inventory();
 $(function(){
-    new item().jq.on('click', function(){
+    $('body').on('click', '.item', function () {
+        inv.inTrans = $(this).clone();
         let par = $(this).parent()
         if (par.attr('id')=='source') {
             show($('.inventory-dialog.get-in'));
         } else if (par.attr('id')=='inventory') {
             show($('.inventory-dialog.get-out'));
+            $('.quantity').val(inv.getCurrQuantity())
         }
-        $(this).clone().appendTo($('.selection'))
+        let selection = inv.inTrans.clone().removeClass('item');
+        selection.appendTo($('.selection'));
         show($('#overlay'));
     });
-    $('.exit').on('click', function(){
+    $('.submit-btn').on('click', function () {
+        let it = inv.inTrans;
+        let qua = $(this).siblings('.quantity').val();
+        inv.addItem(it, Number(qua));
+        console.log(inv.inventory);
+    });
+    $('#target').on('click', function () {
+        hide($('#source'));
+        show($('#inventory'));
+        show($('#target-dialog'));
+    });
+    $('.exit').on('click', function () {
         let par = $(this).parent()
         hide(par);
         if (par.attr('id')=='target-dialog') {
@@ -71,17 +99,5 @@ $(function(){
             $('.selection').empty();
         }
     })
-    $('#target').on('click', function(){
-        hide($('#source'));
-        show($('#inventory'));
-        show($('#target-dialog'));
-    });
 })
 
-inv = new inventory(); //sukuriu nają inventory objektą. Jis turi kol kas vieną atributą, tuščią masyvą 'inventory' ir vieną metodą pridėti daiktų į tą masyvą.
-comp = new item($('#compass70')); //sukuriu du item objektus, kuriuos vėliau pridėsiu į tą savo masyvą. Item objektai iš esmės tėra jqery ojektas su papildomu svorio atributu, kurį kostruktorius nuskaito automatiškai
-wat = new item($('#water500')); 
-inv.addItem(comp, 2); // pridedu vieną
-console.log(inv);
-inv.addItem(wat, 3); //pridedu kitą
-console.log(inv); //IR PO VELNIŲ, ABU CONSOLE.LOG() MAN RODO TĄ PATĮ GALUTINĮ REZULTATĄ. Net jei antras item prie inventory nepridėtas! Kodėl?
